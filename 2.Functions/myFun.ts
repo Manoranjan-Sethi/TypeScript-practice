8ee6d86bfee56e8a815c41f6149fd617503db8b9
Manoranjan-Sethi <manoranjansethi97@gmail.com>
2023-02-08 12:14:29 +0530

    function-2

--- 2.Functions/myFun.ts
+++ 2.Functions/myFun.ts
@@ -1,5 +1,7 @@
-function add(num: number) {
-  return num + 2;
+function add(num: number): number {
+  //tells us that the output must me a number
+  return num + 2; //should be
+  // return "something"; //not giving me error
 }
 
 add(8);
@@ -19,5 +21,25 @@ let logIn = (name: string, password: string, isPaid: boolean = false) => {
 };
 
 logIn("mano", "spartan@123"); //taken false by default
+
+const getMessage = (msg: string): string => {
+  return "this is my message";
+};
+
+const heros = ["thor", "batman", "hulk"];
+
+heros.map((hero): string => {
+  //will always returns a string as output
+  return `Hero is ${hero}`;
+});
+
+function error(msg: string): void {
+  console.log(msg);
+}
+
+function handleError(err: string): never {
+  //termination of the process - never
+  throw new Error(err);
+}
 
 export {};
